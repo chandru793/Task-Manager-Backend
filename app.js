7291d897765471aa70bf887349b84e8fc5d432bf
chandru793 <67958864+chandru793@users.noreply.github.com>
2023-02-06 09:24:10 +0530

    commit

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 const port = 3000;
 
-app.get(`/hello`, (req, res) => {
-    res.send(`Hello World!`)
-})
+app.use(express.json());
 
-app.listen(port, console.log(`Server is listening at port ${port}`));
+app.use("/api/v1/tasks", tasks);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening at port ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
